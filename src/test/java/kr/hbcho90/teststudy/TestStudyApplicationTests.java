41c4824d6984910f65869ff9ceb88f92afadf6ea
hyunbenny <bluesky3268@naver.com>
2024-12-19 11:12:33 +0900

    Initial commit

---
+++ src/test/java/kr/hbcho90/teststudy/TestStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hbcho90.teststudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
